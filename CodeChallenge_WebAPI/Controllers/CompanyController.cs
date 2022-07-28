faa62a4e17d05ec120f463fb83a141aca5c1e119
AlexisVelazquez22 <alexis.velazquez@unosquare.com>
2022-07-28 18:54:20 -0500

    Delete HomeController, create ProductController, create CompanyController

---
+++ CodeChallenge_WebAPI/Controllers/CompanyController.cs
@@ -0,0 +1,45 @@
+﻿using DB.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace CodeChallenge_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompanyController : Controller
+    {
+        private readonly ICompanyService _companyService;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                return Ok(_companyService.Get());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCompany([FromBody] CompanyRequest model)
+        {
+            try
+            {
+                var company = await _companyService.Add(model);
+                return StatusCode(StatusCodes.Status201Created, company);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ CodeChallenge_WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using DB.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace CodeChallenge_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            try
+            {
+                return Ok(_productService.Get());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody] ProductRequest model)
+        {
+            try
+            {
+                var product = await _productService.Add(model);
+                return StatusCode(StatusCodes.Status201Created, product);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditProduct([FromBody] ProductRequest model, [FromRoute] int id)
+        {
+            try
+            {
+                var editedProduct = await _productService.Edit(model, id);
+                return (editedProduct != null) ? Ok(editedProduct) : NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] int id)
+        {
+            try
+            {
+                var productDeleted = await _productService.Delete(id);
+                return (productDeleted == true) ? NoContent() : NotFound();          
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
