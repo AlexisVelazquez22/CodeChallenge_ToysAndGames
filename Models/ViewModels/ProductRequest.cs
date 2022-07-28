d1e0212069dc16700c3adbad51052ef5961089df
AlexisVelazquez22 <alexis.velazquez@unosquare.com>
2022-07-28 18:44:33 -0500

    Rename properties aplying pascal case, set constraints

--- Models/ViewModels/ProductRequest.cs
+++ Models/ViewModels/ProductRequest.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DB.ViewModels
 {
     public class ProductRequest
     {
-        //Te sugiero que le quites los guiones bajos a los nombres y utilices PascalCase para los nombres de las Propiedades ayuda a mejorar a la lectura, el "Product_id"
-        //se refiere al id del producto
-        public int Product_Id { get; set; }
+        [MaxLength(50)]
+        [Required]
         public string? Name { get; set; }
+
+        [MaxLength(100)]
         public string? Description { get; set; }
-        //aqui le falta la restricción de la edad que tiene la tabla Products que debe ser mayor a cero y menor o igual a 100
+
+        [Range(1, 100)]
         public int AgeRestriction { get; set; }
-        //aqui le falta la restriccion del precio que tiene la tabla Products que debe ser mayor a cero y menor o igual a 1000
+
+        [Range(1, 1000)]
+        [Required]
         public decimal Price { get; set; }
-        //Te sugiero que le quites los guiones bajos a los nombres y utilices PascalCase para los nombres de las Propiedades ayuda a mejorar a la lectura, el "company_id"
-        //se refiere al id de la compañía
-        public int Company_Id { get; set; }
+
+        public int CompanyId { get; set; }
     }
 }
