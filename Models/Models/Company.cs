278ceabd4b25c0dd1e08b78eb47e533d704ed13b
AlexisVelazquez22 <alexis.velazquez@unosquare.com>
2022-07-28 18:46:05 -0500

    Rename properties using PascalCase

--- Models/Models/Company.cs
+++ Models/Models/Company.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DB.Models
 {
     public class Company
     {
-        public int Id { get; set; }
+        public int CompanyId { get; set; }
 
-        //Te sugiero que le quites los guiones bajos a los nombres y utilices PascalCase para los nombres de las Propiedades ayuda a mejorar a la lectura, tambien se entiende que por ser una entidad company, el "companyname"
-        //se refiere al nombre de la compañía, name debería bastar
-        public string Company_Name { get; set; }
+        public string? Title { get; set; }
 
         public List<Product> Product { get; set; }
     }
--- Models/Models/Product.cs
+++ Models/Models/Product.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DB.Models
 {
     public class Product
     {
-        //Te sugiero que le quites los guiones bajos a los nombres y utilices PascalCase para los nombres de las Propiedades ayuda a mejorar a la lectura
-        public int Product_Id { get; set; }
+        public int ProductId { get; set; }
 
         public string? Name { get; set; }
 
@@ -18,11 +11,10 @@ namespace DB.Models
 
         public int AgeRestriction { get; set; }
 
-        public decimal Price { get; set; }
+        public decimal Price { get; set; } 
 
-        //Te sugiero que le quites los guiones bajos a los nombres y utilices PascalCase para los nombres de las Propiedades ayuda a mejorar a la lectura
-        public int Company_Id { get; set; }
-        [Required]
+        public int CompanyId { get; set; }
+
         public Company Company { get; set; } 
     }
 }
