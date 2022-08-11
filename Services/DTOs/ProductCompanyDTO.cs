2275e7184582d643be81d34a93b04daf2f1a0030
AlexisVelazquez22 <alexis.velazquez@unosquare.com>
2022-08-11 14:24:19 -0500

    Create DTO

---
+++ Services/DTOs/ProductCompanyDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOs
+{
+    public class ProductCompanyDTO
+    {
+        public int ProductId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int AgeRestriction { get; set; }
+
+        public int CompanyId { get; set; }
+
+        public string? Title { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
